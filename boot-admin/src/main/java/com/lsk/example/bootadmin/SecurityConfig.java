1f1b710aa4d5f495c4c1d41ab67a66614d823e76
lsk <linshaokang2016@163.com>
2018-08-13 21:05:25 +0800

    admin server

--- boot-admin/src/main/java/com/lsk/example/bootadmin/SecurityConfig.java
+++ boot-admin/src/main/java/com/lsk/example/bootadmin/SecurityConfig.java
@@ -1,16 +1,16 @@
-package com.lsk.example.bootweb.config;
+package com.lsk.example.bootadmin;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll()
+        http.authorizeRequests().antMatchers("/instances", "/actuator/**").permitAll()
+                .anyRequest().authenticated().and().httpBasic()
                 .and().csrf().disable();
     }
-
 }
